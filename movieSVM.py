23a1ead0f5aed94cf375a96446b71a5ba574f738
SonyFriend <53859597+SonyFriend@users.noreply.github.com>
2019-08-08 11:04:15 +0800

    Update movieSVM.py

--- movieSVM.py
+++ movieSVM.py
@@ -1,3 +1,7 @@
+#Original data size is 45456,but we interested the movie which is made in USA. 
+#So the data size became 20920. We remove missing value from 'budget' and 'revenue', the data size became 4381.
+#We want to predict the movie will have sequel or not.
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,18 +12,20 @@ df=open('C:/Users/user/Downloads/pythonCode/movie_4381.csv')
 movie=pd.read_csv(df)
 series_row=movie.iloc[:,1]
 series=[]
+#If the movie have sequel,representing 1,otherwise 0.
 for i in range(0,len(movie)):
 	if int(isinstance(series_row[i],float))==1:
 		series.append(0)
 	else:
 		series.append(1)
-movie['series']=series
+movie['series']=series#which is our response(Y)
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn import metrics
 accurate=[]
-for i in range(0,20):
+for i in range(20):
+	#We split the data into training set and test set,80% to be training,20% to be test.
 	x_train,x_test,y_train,y_test=train_test_split(movie[['budget','revenue','popularity','runtime']],movie['series'],test_size=0.2)
 	sc=StandardScaler()
 	sc.fit(x_train.astype(float))
@@ -30,5 +36,5 @@ for i in range(0,20):
 	#print(metrics.classification_report(y_test,svm.predict(x_test_std)))
 	mat=metrics.confusion_matrix(y_test,svm.predict(x_test_std))
 	accurate.append((mat[0,0]+mat[1,1])/sum(sum(mat)))
-print(svm.predict(x_test_std))
-print(np.average(accurate))
+print(svm.predict(x_test_std)) #which is the result for prediction
+print(np.average(accurate))#which is accuracy
